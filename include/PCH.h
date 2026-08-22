03d1991f46de4b7f3e25b6859bf17c7c86764488
doodlum <15017472+doodlum@users.noreply.github.com>
2026-08-22 21:34:23 +0100

    Fix SKSE AE structure compatibility metadata

--- include/PCH.h
+++ include/PCH.h
@@ -121,7 +121,7 @@ extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() noexcept {
 	v.PluginName(Plugin::NAME.data());
 	v.PluginVersion(Plugin::VERSION);
 	v.UsesAddressLibrary();
-	v.UsesNoStructs();
+	v.UsesUpdatedStructs();
 	return v;
 }();
 
